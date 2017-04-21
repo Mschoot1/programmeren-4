62271b4b0a80ee4e581ca89b15649479cefdb236
marnix van der schoot <marnix van der schoot>
2017-04-21 12:28:30 +0200

    Commit four

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || 3000;
+
 app.get('/', function(request, response) {
     response.send('Hello Avans!');
 });
@@ -20,11 +22,32 @@ app.put('/', function(request, response) {
     response.send('Hello Avans, PUT request received!');
 });
 
+app.get('/json', function(request, response) {
+    response.json({
+        'some_name': 'Value',
+        'an_array_of_objects': [
+            {
+                'another_name': 'Another value',
+                'a_further_name': "A further value"
+            },
+            {'yet_another_name': 'Yet another value'}
+        ],
+        'some_boolean': true,
+        'some_integer': 42,
+        'array_of_ints': [
+            2, 3, 5, 7, 11, 13
+        ],
+        'array_of_strings': [
+            "twee", "drie", "vijf", "zeven"
+        ]
+    })
+});
+
 app.get('*', function(request, response) {
     response.status(404);
     response.send('404 - Not found');
 });
 
-app.listen(3000, function() {
-    console.log('Server app is listening on port 3000');
+app.listen(port, function() {
+    console.log('Server app is listening on port ' + port);
 });
