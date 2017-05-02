00e0cab60ef48f7a3475a9b8d69d397e5f6ca830
marnix van der schoot <marnix van der schoot>
2017-05-02 09:34:29 +0200

    commit six

--- index.js
+++ index.js
@@ -24,6 +24,28 @@ app.put('/', function(request, response) {
 
 app.get('/json', function(request, response) {
     response.json({
+        'some_name': 'Value',
+        'an_array_of_objects': [
+            {
+                'another_name': 'Another value',
+                'a_further_name': "A further value"
+            },
+            {'yet_another_name': 'Yet another value'}
+        ],
+        'some_boolean': true,
+        'some_integer': 42,
+        'array_of_ints': [
+            2, 3, 5, 7, 11, 13
+        ],
+        'array_of_strings': [
+            "twee", "drie", "vijf", "zeven"
+        ]
+    })
+});
+
+app.get('/api', function(request, response) {
+
+    var results = {
 
         "results": [
             {
@@ -219,7 +241,11 @@ app.get('/json', function(request, response) {
             "version": "1.1"
         }
 
-    })
+    };
+
+    var i = Math.floor((Math.random() * results.getJSONArray("results").length) + 1);
+    
+    response.json(results.getJSONArray("results").get(i));
 });
 
 app.get('*', function(request, response) {
